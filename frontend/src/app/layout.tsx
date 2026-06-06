0fb687dd0545a226dde9d91c495b3bd0ca9d3752
Pavel <pt1999@mail.ru>
2026-06-06 18:16:03 +0300

    configure frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,23 +1,20 @@
 import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
-import { Providers } from "@/components/layout/Providers";
 import "../styles/globals.css";
 import { ReactNode } from "react";
 
 export const metadata = {
-  title: "Pulse CRUD demo",
-  description: "Пример CRUD приложения для Pulse",
+  title: "Polya Pasha",
+  description: "Приложение для выбора места для свиданий",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ru">
       <body className="flex flex-col min-h-screen px-8">
-        <Providers>
-          <Header />
-          {children}
-          <Footer />
-        </Providers>
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
