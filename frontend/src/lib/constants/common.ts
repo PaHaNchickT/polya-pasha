3b042d08e3a02ca9c14d7627411f93f0ab407fe7
Pavel <pt1999@mail.ru>
2026-06-13 15:23:35 +0300

    feat: add Map page

--- frontend/src/lib/constants/common.ts
+++ frontend/src/lib/constants/common.ts
@@ -5,11 +5,13 @@ export const HEADER_TABS: (keyof typeof HEADER_TABS_MAP)[] = [
   "places",
   "reviews",
   "randomizer",
+  "map",
 ];
 export const HEADER_TABS_MAP = {
   places: "Места",
   reviews: "Отзывы",
   randomizer: "Рандомайзер",
+  map: "Карта",
 };
 
 export const BREADCRUMBS_MAP = {
